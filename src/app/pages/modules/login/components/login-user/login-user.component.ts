54693b026ba065af2febe22796b1470aad9d0a27
aledguedes <alexandredguedes@gmail.com>
2023-03-29 20:41:33 -0300

    loginService

--- src/app/pages/modules/login/components/login-user/login-user.component.ts
+++ src/app/pages/modules/login/components/login-user/login-user.component.ts
@@ -1,4 +1,8 @@
+import { LoginService } from './../../../../../shared/services/login/login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { SnackService } from 'src/app/shared/services/snack/snack.service';
 
 @Component({
   selector: 'app-login-user',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUserComponent implements OnInit {
 
-  constructor() { }
+  login!: FormGroup;
+  hide: boolean = true;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private snack: SnackService,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    this.login = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+
+  authLogin(user: any) {
+    this.loginService.autenticatedUser(user).subscribe({
+      next: (data: any) => {
+        this.loginService.sucessLogin(data.token);
+        this.router.navigate(['/control-panel']);
+      },
+      error: (err) => {
+        console.log('ERR LOGIN', err);
+        // this.toastr.error('Credenciais inválidas!', 'Toastr fun!');
+        this.snack.snack('Credenciais inválidas!', 'error');
+      }
+    });
+  }
+
 
 }
